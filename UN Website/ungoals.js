870b369a96ba277721947a71d3613b5c27bd00ca
Justin Lee <justin.lee22@Y22-Design-Justin-Lee.local>
2019-04-12 20:26:01 -0400

    update

---
+++ UN Website/ungoals.js
@@ -0,0 +1,10 @@
+ var myData = "";
+    var staticUrl = 'https://raw.githubusercontent.com/hyunwookjleeucc/hyunwookjleeucc.github.io/master/UN%20Website/tornado.json'; 
+    $.getJSON(staticUrl, function(data) {
+        
+        console.log(data)
+        iw = window.innerWidth - 100;
+        
+        
+        
+});
